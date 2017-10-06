758bcfcf87acdacc532c311acc3e88aa971ef164
Borrego <joao.borrego64@gmail.com>
2017-10-06 15:36:40 +0100

    add initial empty plugin

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # gazebo-utils
 Set of tools to interact with gazebo simulator
+
+### Object spawner
+
+#### Compilation
+
+Clone the repository to your workspace directory.
+
+```
+cd ~/workspace/gazebo-utils/object-spawner/build &&
+cmake ../ &&
+make
+```
+
+#### Executing
+
+```
+cd ~/workspace/gazebo-utils/object-spawner/ &&
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:`pwd`/build &&
+gzserver spawner.world --verbose
+```
---
+++ object_spawner/object_spawner.cc
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * @brief
+ * 
+ * 
+ * 
+ * @author João Borrego
+ */
+
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+    class WorldPluginTutorial : public WorldPlugin
+    {
+    public: WorldPluginTutorial() : WorldPlugin()
+        {
+          printf("Hello World!\n");
+        }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+        {
+        }
+    };
+    GZ_REGISTER_WORLD_PLUGIN(WorldPluginTutorial)
+}

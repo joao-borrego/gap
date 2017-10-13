6104e0bb745eefadfbb893c417d9716ef64de898
ruipimentelfigueiredo <ruihortafigueiredo@gmail.com>
2017-10-13 18:39:26 +0100

    write generated textures to specified folder

--- pattern_generation/src/tests/pattern_generation_test.cpp
+++ pattern_generation/src/tests/pattern_generation_test.cpp
@@ -3,8 +3,9 @@
 int main()
 {
     PatternGeneration pattern_generation;
-
-    for(int i=0; i<1000000;++i)
+    /* Initialize random seed */
+    srand(time(NULL));
+    for(int i=0; i<1000;++i)
     {
         int img_size= 500;
         int squares = rand() % 20 + 8;     // v2 in the range 1 to 100
@@ -12,7 +13,7 @@ int main()
         if(blockSize%2==0) blockSize++;
 
         //int blockSize = rand() % 25 + 75;     // v2 in the range 1 to 100
-        std::cout << "blocksize: "<< blockSize << std::endl;
+        //std::cout << "blocksize: "<< blockSize << std::endl;
         //int blockSize=75;
         //int squares=8;
         cv::Scalar color1=cv::Scalar( rand()%255, rand()%255, rand()%255);
@@ -20,21 +21,25 @@ int main()
         cv::Scalar color3=cv::Scalar( rand()%255, rand()%255, rand()%255);
 
         cv::Mat chessBoard=pattern_generation.getChessTexture(color1,color2,blockSize,squares);
-
-        cv::imshow("Chess board", chessBoard);
-        cv::waitKey(10);                                          // Wait for a keystroke in the window
+ 	cv::imwrite("/home/rui/shapes/materials/textures/chess_"+std::to_string(i)+".jpg", chessBoard );
+        
+	cv::imshow("Chess board", chessBoard);
+        //cv::waitKey(10);                                          // Wait for a keystroke in the window
 
 
         cv::Mat flatTexture=pattern_generation.getFlatTexture(color3,img_size);
-
+ 	cv::imwrite("/home/rui/shapes/materials/textures/flat_"+std::to_string(i)+".jpg", flatTexture );
 
         cv::imshow("Flat texture", flatTexture);
-        cv::waitKey(10);                                          // Wait for a keystroke in the window
+        //cv::waitKey(10);                                          // Wait for a keystroke in the window
 
         cv::Mat gradientTexture=pattern_generation.getGradientTexture(color1,color2,img_size,false);
+ 	cv::imwrite("/home/rui/shapes/materials/textures/gradient_"+std::to_string(i)+".jpg", gradientTexture );
 
         cv::imshow("Gradient texture", gradientTexture);
-        cv::waitKey(10);                                          // Wait for a keystroke in the window
+        //cv::waitKey(10);                                          // Wait for a keystroke in the window
+
+
     }
 
     return 0;

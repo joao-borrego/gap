b04327841f344a9d566ba4b4a81c50b26b8c9f6f
ruipimentelfigueiredo <ruihortafigueiredo@gmail.com>
2017-10-13 16:24:43 +0100

    texture generation library

---
+++ pattern_generation/include/pattern_generation/PatternGeneration.h
@@ -0,0 +1,17 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+class PatternGeneration
+{
+	public:
+                PatternGeneration()
+                {};
+
+
+                cv::Mat getChessTexture(const cv::Scalar & color1, const cv::Scalar & color2, int blockSize=75, int squares=8);
+                cv::Mat getFlatTexture(const cv::Scalar & color, const int & imageSize);
+                cv::Mat getGradientTexture(const cv::Scalar & color1, const cv::Scalar & color2, const int & imageSize, bool vertical=true);
+};
---
+++ pattern_generation/src/tests/pattern_generation_test.cpp
@@ -0,0 +1,41 @@
+#include "pattern_generation/PatternGeneration.h"
+
+int main()
+{
+    PatternGeneration pattern_generation;
+
+    for(int i=0; i<1000000;++i)
+    {
+        int img_size= 500;
+        int squares = rand() % 20 + 8;     // v2 in the range 1 to 100
+        int blockSize=img_size/squares;
+        if(blockSize%2==0) blockSize++;
+
+        //int blockSize = rand() % 25 + 75;     // v2 in the range 1 to 100
+        std::cout << "blocksize: "<< blockSize << std::endl;
+        //int blockSize=75;
+        //int squares=8;
+        cv::Scalar color1=cv::Scalar( rand()%255, rand()%255, rand()%255);
+        cv::Scalar color2=cv::Scalar( rand()%255, rand()%255, rand()%255);
+        cv::Scalar color3=cv::Scalar( rand()%255, rand()%255, rand()%255);
+
+        cv::Mat chessBoard=pattern_generation.getChessTexture(color1,color2,blockSize,squares);
+
+        cv::imshow("Chess board", chessBoard);
+        cv::waitKey(10);                                          // Wait for a keystroke in the window
+
+
+        cv::Mat flatTexture=pattern_generation.getFlatTexture(color3,img_size);
+
+
+        cv::imshow("Flat texture", flatTexture);
+        cv::waitKey(10);                                          // Wait for a keystroke in the window
+
+        cv::Mat gradientTexture=pattern_generation.getGradientTexture(color1,color2,img_size,false);
+
+        cv::imshow("Gradient texture", gradientTexture);
+        cv::waitKey(10);                                          // Wait for a keystroke in the window
+    }
+
+    return 0;
+}

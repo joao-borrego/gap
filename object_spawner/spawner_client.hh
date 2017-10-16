2c4824a0d818a231481b02714d3ba24fbbd9dc34
Borrego <joao.borrego64@gmail.com>
2017-10-17 00:56:27 +0100

    add code to map textures to object in client
    Gazebo seems to cache the script files, so only the first texture is properly loaded.
    Perhaps it is needed to have a script file for each texture.

--- object_spawner/spawner_client.hh
+++ object_spawner/spawner_client.hh
@@ -1,6 +1,16 @@
+/* Gazebo */
 #include <gazebo/gazebo_config.h>
 #include <gazebo/transport/transport.hh>
 #include <gazebo/msgs/msgs.hh>
+
+/* I/O streams */
+#include <iostream>
+/* File streams */
+#include <fstream>
+/* Regular expressions */
+#include <regex>
+/* For iterating over the contents of a dir */
+#include <boost/filesystem.hpp>
 
 /* Custom messages */
 #include "object_spawner_request.pb.h"
@@ -23,3 +33,14 @@
 #define SPHERE object_spawner_msgs::msgs::SpawnRequest::SPHERE
 #define CYLINDER object_spawner_msgs::msgs::SpawnRequest::CYLINDER
 #define BOX object_spawner_msgs::msgs::SpawnRequest::BOX
+
+#define OBJECT_SPAWNER_TOPIC "~/gazebo-utils/object_spawner"
+
+#define MODEL_NAME "model"
+#define MODEL_DIR "../../media/" MODEL_NAME
+#define TEXTURES_DIR MODEL_DIR "/materials/textures/"
+#define SCRIPT_DIR MODEL_DIR "/materials/scripts/"
+#define SCRIPT_FILE SCRIPT_DIR MODEL_NAME ".material"
+
+#define TEXTURE_URI "model://model/materials/scripts</uri>\
+                    <uri>model://model/materials/textures"
--- pattern_generation/src/tests/pattern_generation_test.cpp
+++ pattern_generation/src/tests/pattern_generation_test.cpp
@@ -1,45 +1,81 @@
+#include <sstream>
+#include <string>
+
 #include "pattern_generation/PatternGeneration.h"
+
+/* Image output directory */
+#define OUTPUT_DIR "../../media/model/materials/textures"
+/* Show image GUI */
+#define SHOW_IMGS false
+/* Number of images per class of texture */
+#define IMGS_PER_CLASS 1000
+
 
 int main()
 {
     PatternGeneration pattern_generation;
+    
     /* Initialize random seed */
     srand(time(NULL));
-    for(int i=0; i<1000;++i)
+    
+    int img_size = 500;
+    int squares;
+    int block_size;
+
+    for (int i = 0; i < IMGS_PER_CLASS; ++i)
     {
-        int img_size= 500;
-        int squares = rand() % 20 + 8;     // v2 in the range 1 to 100
-        int blockSize=img_size/squares;
-        if(blockSize%2==0) blockSize++;
-
-        //int blockSize = rand() % 25 + 75;     // v2 in the range 1 to 100
-        //std::cout << "blocksize: "<< blockSize << std::endl;
-        //int blockSize=75;
-        //int squares=8;
-        cv::Scalar color1=cv::Scalar( rand()%255, rand()%255, rand()%255);
-        cv::Scalar color2=cv::Scalar( rand()%255, rand()%255, rand()%255);
-        cv::Scalar color3=cv::Scalar( rand()%255, rand()%255, rand()%255);
-
-        cv::Mat chessBoard=pattern_generation.getChessTexture(color1,color2,blockSize,squares);
- 	cv::imwrite("/home/rui/shapes/materials/textures/chess_"+std::to_string(i)+".jpg", chessBoard );
+        /* Generate square texture */
         
-	cv::imshow("Chess board", chessBoard);
-        //cv::waitKey(10);                                          // Wait for a keystroke in the window
+        squares = rand() % 20 + 8;     // v2 in the range 1 to 100
+        block_size = img_size / squares;
+        if (block_size % 2 == 0)
+            block_size++;
 
+        cv::Scalar color1 = cv::Scalar(rand()%255, rand()%255, rand()%255);
+        cv::Scalar color2 = cv::Scalar(rand()%255, rand()%255, rand()%255);
+        cv::Scalar color3 = cv::Scalar(rand()%255, rand()%255, rand()%255);
 
-        cv::Mat flatTexture=pattern_generation.getFlatTexture(color3,img_size);
- 	cv::imwrite("/home/rui/shapes/materials/textures/flat_"+std::to_string(i)+".jpg", flatTexture );
+        cv::Mat chess_board = pattern_generation.getChessTexture(color1, color2, block_size, squares);
+        std::stringstream img_filename;
+        img_filename << OUTPUT_DIR << "/chess_" << std::to_string(i) << ".jpg";
+        if (!cv::imwrite(img_filename.str(), chess_board)){
+            std::cout << "[ERROR] Could not save " << img_filename.str() <<
+            ". Please ensure the destination folder exists!" << std::endl; 
+            exit(EXIT_FAILURE);
+        }
+        
+        if (SHOW_IMGS)
+            cv::imshow("Chess board", chess_board);
 
-        cv::imshow("Flat texture", flatTexture);
-        //cv::waitKey(10);                                          // Wait for a keystroke in the window
+        /* Generate flat texture */
 
-        cv::Mat gradientTexture=pattern_generation.getGradientTexture(color1,color2,img_size,false);
- 	cv::imwrite("/home/rui/shapes/materials/textures/gradient_"+std::to_string(i)+".jpg", gradientTexture );
+        cv::Mat flat_texture = pattern_generation.getFlatTexture(color3,img_size);
+        
+        img_filename.str(std::string());
+        img_filename << OUTPUT_DIR << "/flat_" << std::to_string(i) << ".jpg";
+        if (!cv::imwrite(img_filename.str(), flat_texture)){
+            std::cout << "[ERROR] Could not save " << img_filename.str() <<
+            ". Please ensure the destination folder exists!" << std::endl; 
+            exit(EXIT_FAILURE);
+        }
 
-        cv::imshow("Gradient texture", gradientTexture);
-        //cv::waitKey(10);                                          // Wait for a keystroke in the window
+        if (SHOW_IMGS)
+            cv::imshow("Flat texture", flat_texture);
 
+        /* Generate gradient texture */
 
+        cv::Mat gradient_texture = pattern_generation.getGradientTexture(color1, color2, img_size, false);
+        
+        img_filename.str(std::string());
+        img_filename << OUTPUT_DIR << "/gradient_" << std::to_string(i) << ".jpg";
+        if (!cv::imwrite(img_filename.str(), gradient_texture)){
+        std::cout << "[ERROR] Could not save " << img_filename.str() <<
+            ". Please ensure the destination folder exists!" << std::endl; 
+            exit(EXIT_FAILURE);
+        }
+
+        if (SHOW_IMGS)
+            cv::imshow("Gradient texture", gradient_texture);
     }
 
     return 0;

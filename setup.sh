c5d5a339bcba95816099c6d6797f2b5a29d4d352
Borrego <joao.borrego64@gmail.com>
2017-10-25 21:28:03 +0100

    cleanup file structure and add top CMakeLists
    Reorganised some of the code.
    Created a top level CMakeLists.txt which can build the whole project.
    Updated readme files.

--- setup.sh
+++ setup.sh
@@ -4,8 +4,14 @@
 source /usr/share/gazebo-7/setup.sh &>/dev/null
 source /usr/share/gazebo-8/setup.sh &>/dev/null
 # Export location of object spawner gazebo plugin
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:`pwd`/object_spawner/build
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:`pwd`/build/object_spawner
 # Export location of camera tools gazebo plugin
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:`pwd`/camera/build
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:`pwd`/build/camera
 # Export location of custom resources
 export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:`pwd`/media
+# Export location of custome models
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:`pwd`/models
+# Export gazebo-utils location
+export GAZEBO_UTILS_PATH=`pwd`
+
+echo "gazebo-utils was successfully initialised."

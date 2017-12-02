a3c3681d8a3caeb6d7a49d560cf880f3c40a8def
Rui Figueiredo <ruihortafigueiredo@gmail.com>
2017-12-02 17:46:08 +0000

    removed redundant headers

--- capture_example/capture_example.hh
+++ capture_example/capture_example.hh
@@ -37,7 +37,7 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include "opencv2/imgproc.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 /*
  * Macros for custom messages
  */

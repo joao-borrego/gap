14904ae966847b6a0aee80003a487be0bbbcacbe
Borrego <joaohenriqueborrego@gmail.com>
2018-05-08 00:46:44 +0100

    cleanup output structure

--- setup.sh
+++ setup.sh
@@ -5,12 +5,8 @@ source /usr/local/share/gazebo-9/setup.sh
 
 # Libraries
 
-# Export location of camera tools gazebo plugin
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:`pwd`/build/camera_utils
-# Export location of visual tools gazebo plugin
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:`pwd`/build/visual_utils
-# Export location of world utils gazebo plugin
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:`pwd`/build/world_utils
+# Export location of gazebo plugins
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:`pwd`/build/lib
 
 # Resources
 

35403dbacf5cc0cd63b0e8a5d60bcd47b05a03a5
Borrego <joaohenriqueborrego@gmail.com>
2018-05-09 07:49:14 +0100

    remove duplicate file

--- gap-config.cmake
+++
@@ -1,6 +0,0 @@
-# Package configuration file
-
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/gap.cmake)
-install(FILES gap-config.cmake DESTINATION ${main_lib_dest})
-install(EXPORT ...)
